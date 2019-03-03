d4f0d493901aff130afd950476cad21c8ff7ad0a
xiongyayun428@163.com <Xyy42899>
2019-03-03 21:01:41 +0800

    update

--- athena.eureka.discovery/src/main/java/com/xyy/athena/eureka/discovery/EurekaDiscoveryApplication.java
+++ athena.eureka.discovery/src/main/java/com/xyy/athena/eureka/discovery/EurekaDiscoveryApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * EurekaDiscoveryApplication
+ *
+ * @author Yayun.Xiong
+ * @date 2019-03-03 20:57
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaDiscoveryApplication {
