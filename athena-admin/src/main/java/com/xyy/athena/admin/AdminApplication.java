91b2994658d1eb53dc448051b03d39d725f50ac2
xiongyayun428@163.com <Xyy42899>
2019-05-20 00:34:42 +0800

    update

--- athena-admin/src/main/java/com/xyy/athena/admin/AdminApplication.java
+++ athena-admin/src/main/java/com/xyy/athena/admin/AdminApplication.java
@@ -3,6 +3,12 @@ package com.xyy.athena.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * AdminApplication
+ *
+ * @author Yayun.Xiong
+ * @date 2019-05-20
+ */
 @SpringBootApplication
 //@EnableEurekaClient
 //@EnableFeignClients(basePackages = { "com.xyy.athena.role.api", "com.xyy.athena.user" })
