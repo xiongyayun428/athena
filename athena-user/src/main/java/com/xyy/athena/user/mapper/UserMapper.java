606f2954064b15f86ce0e443a6d3a14406f08592
熊亚运 <xiongyayun428@163.com>
2019-06-05 23:33:58 +0800

    update

--- athena-user/src/main/java/com/xyy/athena/user/mapper/UserMapper.java
+++ athena-user/src/main/java/com/xyy/athena/user/mapper/UserMapper.java
@@ -3,15 +3,11 @@ package com.xyy.athena.user.mapper;
 import com.xyy.athena.db.tk.TkMapper;
 import com.xyy.athena.user.model.User;
 
+/**
+ * UserMapper
+ *
+ * @author: XYY
+ * @date: 2019-06-05 17:16
+ */
 public interface UserMapper extends TkMapper<User> {
-//    int deleteByPrimaryKey(String userId);
-//
-//    int insert(User record);
-//
-//    User selectByPrimaryKey(String userId);
-//
-//    Page<User> selectAll(User user);
-//
-//    int updateByPrimaryKey(User record);
-
 }
