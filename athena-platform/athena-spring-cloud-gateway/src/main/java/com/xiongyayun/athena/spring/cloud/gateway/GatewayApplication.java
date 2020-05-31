15894f14b46077ad8ff708e3731c5bc913a772bd
xiongyayun428 <xiongyayun428@163.com>
2020-05-31 23:04:34 +0800

    update

---
+++ athena-platform/athena-spring-cloud-gateway/src/main/java/com/xiongyayun/athena/spring/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.xiongyayun.athena.spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * EurekaServerApplication
+ *
+ * @author Yayun.Xiong
+ * @date 2020/5/30
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
