5627a830d1231ade5227b182f1a8ae6e43bd3bd8
熊亚运 <xiongyayun428@163.com>
2020-12-25 17:25:24 +0800

    init

--- athena-platform/athena-system/src/main/java/com/xiongyayun/athena/system/SystemApplication.java
+++ athena-platform/athena-system/src/main/java/com/xiongyayun/athena/system/SystemApplication.java
@@ -1,9 +1,19 @@
 package com.xiongyayun.athena.system;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.context.annotation.ComponentScan;
@@ -14,14 +24,35 @@ import org.springframework.context.annotation.ComponentScan;
  * @author Yayun.Xiong
  * @date 2019-03-03 16:30
  */
-@SpringBootApplication(scanBasePackages = { "com.xiongyayun.athena" })
-@MapperScan("com.xiongyayun.athena.system.mapper")
-//@EnableEurekaClient
 //@EnableFeignClients(basePackages = { "com.xiongyayun.athena.role.api", "com.xiongyayun.athena.user" })
-public class SystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SystemApplication.class, args);
+@EnableOpenApi
+@SpringBootApplication(scanBasePackages = { "com.xiongyayun.athena" })
+@EnableDiscoveryClient // 开启服务注册发现功能
+//@EnableFeignClients(basePackages = "com.xiongyayun.athena")
+@MapperScan("com.xiongyayun.athena.system.mapper")
+public class SystemApplication {
+	private static final Logger log = LoggerFactory.getLogger(SystemApplication.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		ConfigurableApplicationContext context = SpringApplication.run(SystemApplication.class, args);
+		String name = ManagementFactory.getRuntimeMXBean().getName();
+		String pid = name.split("@")[0];
+		Environment env = context.getEnvironment();
+		String port = env.getProperty("server.port"), contextPath = env.getProperty("server.servlet.context-path", "");
+		log.info("\n--------------------------------------------------------------------------------------------------------------------\n\t" +
+						"Application '{}' is running! Pid: {}, Access URLs:\n\t" +
+						"Local: \t\thttp://localhost:{}{}\n\t" +
+						"External: \thttp://{}:{}{}" +
+						"\n--------------------------------------------------------------------------------------------------------------------",
+				env.getProperty("spring.application.name", context.getId()),
+				pid,
+				port,
+				contextPath,
+				InetAddress.getLocalHost().getHostAddress(),
+				port,
+				contextPath
+		);
 	}
 
 }
